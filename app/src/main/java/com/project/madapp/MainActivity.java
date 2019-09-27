989dd8e080aaa891e234eb87b9dfb83d253d89fb
azza1246 <aaronhusseinazza1997@outlook.com>
2019-09-27 14:32:56 +1000

    Buttons update

--- app/src/main/java/com/project/madapp/MainActivity.java
+++ app/src/main/java/com/project/madapp/MainActivity.java
@@ -3,17 +3,30 @@ package com.project.madapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
+     button button;
+     textView textView;
+     button2 button2;
+     button3 button3;
+     button4 button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewByID(r.id.one);
-        button2 = findViewByID(r.id.two);
-        button3 = findViewByID(r.id.three);
-        button4 = findViewByID(r.id.four);
+        textView = findViewID(r.id.textLMS);
+        button = findViewByID(r.id.Bone);
+        button2 = findViewByID(r.id.Btwo);
+        button3 = findViewByID(r.id.Bthree);
+        button4 = findViewByID(r.id.Bfour);
+
+        button.setOnClickListener(new View.onClickListener() {
+         public void onClick(View v) {
+         }
+        }
+        }
 
 
     }
