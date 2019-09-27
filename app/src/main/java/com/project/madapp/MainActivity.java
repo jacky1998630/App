08d3b1ec2072a2602b754dd0b9faea5fac6ce6c5
azza1246 <aaronhusseinazza1997@outlook.com>
2019-09-27 13:07:15 +1000

    Buttons

--- app/src/main/java/com/project/madapp/MainActivity.java
+++ app/src/main/java/com/project/madapp/MainActivity.java
@@ -10,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+        button = findViewByID(r.id.);
+        button2 = findViewByID(r.id.);
+        button3 = findViewByID(r.id.);
+        button4 = findViewByID(r.id.);
+
+
     }
 }
