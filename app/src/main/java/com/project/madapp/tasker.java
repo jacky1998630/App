a3f28c5432ff9813f53f896798246000c3b530f4
jacky1998630 <jackymel1998@gmail.com>
2019-09-30 17:02:58 +1000

    started todo

---
+++ app/src/main/java/com/project/madapp/tasker.java
@@ -0,0 +1,16 @@
+package com.project.madapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tasker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tasker);
+
+        https://www.codercrunch.com/post/47915445/build-a-todo-app-in-android
+    }
+}
