7dd0bd2252bf5539789094dc3c40e9048cd95e07
azza1246 <aaronhusseinazza1997@outlook.com>
2019-10-01 14:04:30 +1000

    openNewActivity

--- app/src/main/java/com/project/madapp/MainActivity.java
+++ app/src/main/java/com/project/madapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.project.madapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +22,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button Bthree = findViewById(R.id.Bthree);
         Button Bfour = findViewById(R.id.Bfour);
 
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               openActivity2();
+            }
+        });
+            public void openActivity2() {
+                Intent intent = new Intent(this, Activity2.class);
+                startActivity(intent);
+        }
         Bone.setOnClickListener(this);
         Btwo.setOnClickListener(this);
         Bthree.setOnClickListener(this);
