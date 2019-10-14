233a8dc62d55e70c4536e2429a35bc646bc74f84
19332070 <jackymel1998@gmail.com>
2019-10-14 13:00:52 +1100

    table and download page

---
+++ app/src/main/java/com/project/madapp/ui/downloads.java
@@ -0,0 +1,16 @@
+package com.project.madapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.madapp.R;
+
+public class downloads extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_downloads);
+    }
+}
